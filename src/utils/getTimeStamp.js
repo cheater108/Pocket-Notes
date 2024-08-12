9398c1b1f16fee0ee9f6ee3daaeef63ef3b3ecd0
Chetan Anand <chetan108@outlook.com>
2024-08-13 00:10:41 +0530

    fixed timestamp logic

--- src/utils/getTimeStamp.js
+++ src/utils/getTimeStamp.js
@@ -20,7 +20,7 @@ function getTimeStamp(time) {
         hour12: true,
     });
     const date = `${time.getDate()} ${
-        three_letter_month[time.getMonth() - 1]
+        three_letter_month[time.getMonth()]
     } ${time.getFullYear()}`;
     return { clock_time, date };
 }
